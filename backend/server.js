b5677eaa89d849473b471d6fa9cacd0e1a905e3d
ShauryaBehra <shauryabehra23@gmail.com>
2026-03-13 00:02:54 +0530

    cors fixing

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const startServer = async () => {
     app.use(
       cors({
         origin: [
+          "https://quillr.vercel.app",
           "http://localhost:5173",
           "http://localhost:5174",
           "http://localhost:5175",
